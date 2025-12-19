6a107aa2f5ee6d53cc9400ff4d2bd2d3885ed722
SSDR13 <dineshraghavendra13@gmail.com>
2025-12-19 11:42:11 +0530

    Restore coreProgramming folder

---
+++ src/coreProgramming/programmingElements/Level2/FareToCel.java
@@ -0,0 +1,16 @@
+package coreProgramming.programmingElements.Level2;//5.	Temperature Conversion from Fahrenheit to Celsius
+
+import java.util.*;
+public class FareToCel {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the temperature (in °F): ");
+        double fahrenheit = sc.nextDouble();
+        double celsiusResult = (fahrenheit - 32) * (5/9.0);
+
+        System.out.printf("The %.1f fahrenheit is %.2f celsius", fahrenheit, celsiusResult);
+        sc.close();
+    }
+}
+
